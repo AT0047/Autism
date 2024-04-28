94705025611c3b19cfb58b896efd03f0fa61b496
AT0047 <aboddy.net@gmail.com>
2024-04-28 14:56:21 +0300

    "frontend adding"

---
+++ resources/views/frontend/about_me/about_me.blade.php
@@ -0,0 +1,358 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Simone - Creative Personal CV/Resume HTML Template">
+    <meta name="author" content="Paul">
+
+<!-- Favicons -->
+<link rel="shortcut icon" href="favicon.png">
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-114x114.png">
+
+<title>Autism&ME</title>
+
+<!-- Styles -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:500,600,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i&display=swap" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet" media="screen">
+</head>
+<body>
+  <div class="animsition">   
+    <div class="loader">
+      <div class="spinner"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>
+     </div>
+    <!-- Content CLick Capture-->
+
+    <div class="click-capture"></div>
+
+    <!-- Sidebar Menu-->
+
+    <div class="menu"> 
+      <span class="close-menu icon-cross2 right-boxed"></span>
+      <ul class="menu-list right-boxed">
+        <li  data-menuanchor="page1">
+          <a  href="../home-personal.html">Home</a>
+        </li>
+        <li  data-menuanchor="page1">
+          <a  href="../Articles.html">Articles</a>
+        </li>
+      </ul>
+      <div class="menu-footer right-boxed">
+        <div class="social-list">
+          <a href="" class="icon ion-social-twitter"></a>
+          <a href="" class="icon ion-social-facebook"></a>
+          <a href="" class="icon ion-social-googleplus"></a>
+          <a href="" class="icon ion-social-linkedin"></a>
+          <a href="" class="icon ion-social-dribbble-outline"></a>
+        </div>
+        <div class="copy">© Ramsay 2019. All Rights Reseverd</div>
+      </div>
+    </div>
+
+    <!-- Navbar -->
+
+    <header class="navbar navbar-fullpage boxed">
+      <div class="navbar-bg"></div>
+      <a class="brand" href="#">
+        <img alt="" style="width: 50px;" src="favicon.png">
+        <div class="brand-info">
+        </div>
+      </a>
+
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      
+      <div class="contacts d-none d-md-block">
+        <div class="contact-item">
+          +96 56-85-1379
+        </div>
+        <div class="contact-item spacer">
+          /
+        </div>
+        <div class="contact-item">
+          <a href="mailto:contact@Ramsay.com">contact@Ramsay.com</a>
+        </div>
+      </div>
+    </header>
+    <div class="copy-bottom white boxed">© Ramsay 2019.</div>
+    <div class="social-list social-list-bottom boxed">
+        <a href="" class="icon ion-social-twitter"></a>
+        <a href="" class="icon ion-social-facebook"></a>
+        <a href="" class="icon ion-social-googleplus"></a>
+        <a href="" class="icon ion-social-linkedin"></a>
+        <a href="" class="icon ion-social-dribbble-outline"></a>
+      </div>
+    <div class="pagepiling">
+      <div data-anchor="page1" class="pp-scrollable text-white section section-1">
+        <div class="scroll-wrap">
+          <div class="section-bg" style="background-image:url(images/bg/about3.jpg);"></div>
+          <div class="scrollable-content">
+            <div class="vertical-centred">
+              <div class="boxed boxed-inner">
+                <div class="boxed">
+                  <div class="container">
+                    <div class="intro">
+                      <div class="row">
+                        <div class="col-md-8 col-lg-6">
+                          <h1 class="display-2 text-white  wow fadeIn" data-wow-delay="0.1s"><span class="text-primary">Howdy,</span> I'm Ramsay</h1>
+                          <a class="popup-youtube"  href="images/bg/vabout4.mp4"><span class="icon ion-ios-play"></span>Watch video </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      <div data-anchor="page3" class="pp-scrollable text-white section section-3">
+        <div class="scroll-wrap">
+          <div class="section-bg mask" style="background-image:url(images/bg/about6.jpg);"></div>
+          <div class="scrollable-content">
+            <div class="vertical-title d-none d-lg-block"><span>resume</span></div>
+            <div class="vertical-centred">
+              <div class="boxed boxed-inner">
+                <div class="boxed">
+                  <div class="container">
+                    <div class="intro">
+                      <div class="row row-resume">
+                        <div class="col-md-6 fadeY fadeY-1">
+                          <h2 class="resume-header title">Education</h2>
+                          <div class="col-resume">
+                            <div class="resume-content">
+                              <div class="resume-inner">
+                                <div class="resume-row">
+                                  <h6 class="resume-type">Specialization course</h6>
+                                  <i class="resume-study">University of studies, Poland, Cracow<br>Jan 2004 - Dec 2006</i>
+                                  <p class="resume-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus nobis animi assumenda sint recusandae! Dolor placeat debitis animi illum quo repellendus pariatur, enim</p>
+                                </div>
+                                <div class="resume-row">
+                                  <h6 class="resume-type">Academy course</h6>
+                                  <i class="resume-study">University of studies, Poland, Cracow<br>Jan 2004 - Dec 2006</i>
+                                  <p class="resume-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus nobis animi assumenda sint recusandae! Dolor placeat debitis animi illum quo repellendus pariatur, enim </p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col-md-6 pt-md-5 mt-md-5 fadeY fadeY-2">
+                          <h2 class="resume-header title">Experience</h2>
+                          <div class="col-resume">
+                            <div class="resume-content">
+                              <div class="resume-inner">
+                                <div class="resume-row">
+                                  <h6 class="resume-type">WEBDESIGNER & FRONT-END</h6>
+                                  <i class="resume-study">University of studies, Poland, Cracow<br>Jan 2004 - Dec 2006</i>
+                                  <p class="resume-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus nobis animi assumenda sint recusandae! Dolor placeat debitis animi illum quo repellendus pariatur, enim</p>
+                                </div>
+                                <div class="resume-row">
+                                  <h6 class="resume-type">WORDPRESS DEVELOPER</h6>
+                                  <i class="resume-study">University of studies, Poland, Cracow<br>Jan 2004 - Dec 2006</i>
+                                  <p class="resume-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus nobis animi assumenda sint recusandae! Dolor placeat debitis animi illum quo repellendus pariatur, enim</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                       </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     
+      <div data-anchor="page5" class="pp-scrollable text-white section section-5">
+        <div class="scroll-wrap">
+          <div class="bg-changer">
+            <div class="section-bg" style="background-image:url(images/bg/about2.jpg);"></div>
+            <div class="section-bg" style="background-image:url(images/bg/about3.jpg);"></div>
+            <div class="section-bg" style="background-image:url(images/bg/about5.jpg);"></div>
+            <div class="section-bg" style="background-image:url(images/bg/about6.jpg);"></div>
+          </div>
+          <div class="scrollable-content">
+            <div class="vertical-title  d-none d-lg-block"><span>my works</span></div>
+            <div class="vertical-centred">
+              <div class="boxed boxed-inner">
+                <div class="boxed">
+                  <div class="container">
+                    <div class="intro">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="project-row">
+                            <a class="active" href="#">
+                              <span class="project-number">01</span>
+                              <h2 class="project-title">Abstract Skat</h2>
+                              <div class="project-category">Illustration</div>
+                            </a>
+                          </div>
+                          <div class="project-row">
+                            <a href="#">
+                              <span class="project-number">02</span>
+                              <h2 class="project-title">Borato Prism</h2>
+                              <div class="project-category">Branding</div>
+                            </a>
+                          </div>
+                          <div class="project-row">
+                            <a href="#">
+                              <span class="project-number">03</span>
+                              <h2 class="project-title">Brole Mobile App</h2>
+                              <div class="project-category">Mobile Design</div>
+                            </a>
+                          </div>
+                          <div class="project-row">
+                            <a href="#">
+                              <span class="project-number">04</span>
+                              <h2 class="project-title">Bauhaus Studio</h2>
+                              <div class="project-category">House Design</div>
+                            </a>
+                          </div>
+                          <div class="view-all view-all-projects">
+                            <a  href="#">
+                              View all projects
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+      <div data-anchor="page7" class="pp-scrollable text-white section section-7">
+        <div class="scroll-wrap">
+          <div class="section-bg" style="background-image:url(images/bg/about1.jpg);"></div>
+          <div class="bg-quote"></div>
+          <div class="scrollable-content">
+            <div class="vertical-title  d-none d-lg-block"><span>testimonials</span></div>
+            <div class="vertical-centred">
+              <div class="boxed boxed-inner">
+                <div class="boxed">
+                  <div class="container">
+                    <div class="intro">
+                      <div class="review-carousel owl-carousel">
+                        <div class="review-carousel-item">
+                          <div class="review-row">
+                            <div class="row">
+                              <div class="col-md-5">
+                                <div class="review-author">
+                                  <div class="author-name">David & Elisa</div>
+                                  <i>Apartment view lake at Brooklyn</i>
+                                </div>
+                              </div>
+                              <div class="col-md-7 text">
+                              <p>If you are seeking an Interior designer that will understand exactly your needs, and someone who will utilise their creative and technical skills in parity with your taste, then Suzanne at The Ramsay Studio is perfect.</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="review-row">
+                            <div class="row">
+                              <div class="col-md-5">
+                                <div class="review-author">
+                                  <div class="author-name">Amanda</div>
+                                  <i>Apartment view lake at Brooklyn</i>
+                                </div>
+                              </div>
+                              <div class="col-md-7 text">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio dolorem reiciendis doloremque veniam perspiciatis quam velit pariatur eius, repellendus dolores eveniet maiores sed. Quod quam minus dolore sed cumque aliquid.</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="review-carousel-item">
+                          <div class="review-row">
+                            <div class="row">
+                              <div class="col-md-5">
+                                <div class="review-author">
+                                  <div class="author-name">David & Elisa</div>
+                                  <i>Apartment view lake at Brooklyn</i>
+                                </div>
+                              </div>
+                              <div class="col-md-7 text">
+                              <p>If you are seeking an Interior designer that will understand exactly your needs, and someone who will utilise their creative and technical skills in parity with your taste, then Suzanne at The Ramsay Studio is perfect.</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="review-row">
+                            <div class="row">
+                              <div class="col-md-5">
+                                <div class="review-author">
+                                  <div class="author-name">Amanda</div>
+                                  <i>Apartment view lake at Brooklyn</i>
+                                </div>
+                              </div>
+                              <div class="col-md-7 text">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio dolorem reiciendis doloremque veniam perspiciatis quam velit pariatur eius, repellendus dolores eveniet maiores sed. Quod quam minus dolore sed cumque aliquid.</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="review-carousel-item">
+                          <div class="review-row">
+                            <div class="row">
+                              <div class="col-md-5">
+                                <div class="review-author">
+                                  <div class="author-name">David & Elisa</div>
+                                  <i>Apartment view lake at Brooklyn</i>
+                                </div>
+                              </div>
+                              <div class="col-md-7 text">
+                              <p>If you are seeking an Interior designer that will understand exactly your needs, and someone who will utilise their creative and technical skills in parity with your taste, then Suzanne at The Ramsay Studio is perfect.</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="review-row">
+                            <div class="row">
+                              <div class="col-md-5">
+                                <div class="review-author">
+                                  <div class="author-name">Amanda</div>
+                                  <i>Apartment view lake at Brooklyn</i>
+                                </div>
+                              </div>
+                              <div class="col-md-7 text">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio dolorem reiciendis doloremque veniam perspiciatis quam velit pariatur eius, repellendus dolores eveniet maiores sed. Quod quam minus dolore sed cumque aliquid.</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+<!-- jQuery -->
+
+<script src="js/jquery.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/animsition.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.pagepiling.min.js"></script>
+
+<!-- Scripts -->
+<script src="js/scripts.js"></script> 
+</body>
+</html>
