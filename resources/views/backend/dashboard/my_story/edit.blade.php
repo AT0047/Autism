7c5d5d4f0c7cff4e828ffd3bc5b5cb37a5972459
AT0047 <aboddy.net@gmail.com>
2024-05-01 23:33:14 +0300

    "My Story Backend Is Done"

---
+++ resources/views/backend/dashboard/my_story/edit.blade.php
@@ -0,0 +1,60 @@
+@extends('backend.dashboard.dashboard')
+@section('content')
+    <div class="row">
+        <div class="col-xl-10 order-xl-1">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class="mb-0">Edit</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('mystory.update', $mystory->id)}}" method ="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('patch')
+                        <h6 class="heading-small text-muted mb-4">Information</h6>
+                            <div class="pl-lg-4">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-username">Title 1</label>
+                                            <input type="text" value="{{old('title1', $mystory->title1)}}" name="title1" class="form-control" placeholder="Write your title here">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pl-lg-4">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="form-group">
+                                            <label class="form-control-label" for="input-username">Title 2</label>
+                                            <input type="text" value="{{old('title1', $mystory->title2)}}" name="title2" class="form-control" placeholder="Write your title here">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <div class="pl-lg-4 ">
+                            <div class="form-group ">
+                                <label class="form-control-label ">Content</label>
+                                <textarea id="postContent" name="content" rows="15" class="form-control">{{old('content', $mystory->content)}}</textarea>
+                            </div>
+                        </div>
+                        <div class="pl-lg-4 ">
+                            <label class="form-control-label ">Upload Image</label>
+                            <div class="input-group">
+                                <input type="file" name="photo" class="form-control">
+                            </div>
+                            <img width = "150" height = "100" src="{{asset('dashboard/img/'. $mystory->photo)}}"/>
+                        </div>
+                        <div class="d-flex mt-3 justify-content-end">
+                            <a href="posts.php" class="btn btn-secondary">Back</a>
+                            <button type="submit" class="btn btn-success">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
